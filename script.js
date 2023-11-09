31591de3612e6a85c75b8335f388c2fae68ab242
chrisprince-ic <68997874+chrisprince-ic@users.noreply.github.com>
2023-11-09 12:07:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let iconframe = document.getElementById("iconsframeworks");
+let cardhover = document.getElementById("cardhover");
+
+function makevisible(){
+  
+      iconframe.style.visibility = "visible";  
+      iconframe.style.display = "flex";
+}
+function makeinvisible(){
+    iconframe.style.visibility = "hidden";  
+      iconframe.style.display = "none";
+}
